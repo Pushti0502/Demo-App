2523d91f30e8d4e0bc6bf5a3fa5022fe8cc0fc76
Pushti0502 <pushti.shah@technostacks.com>
2024-05-20 17:02:15 +0530

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
+import About from './About'
+import Home from './Home'
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+        
+      },
+      {
+        path: 'about',
+        element:<About/>
+        
+      },
+     
+    ]
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
